874b2587f76f96647c123835c9de93fafa7b0090
michele <michele.damico@gmail.com>
2023-03-19 18:30:24 +0100

    The fast version enable

--- src/lib.rs
+++ src/lib.rs
@@ -36,41 +36,41 @@ mod tests {
             .unwrap()
     }
 
-    // #[rstest]
-    // #[allow(unnameable_test_items)]
-    // fn fast(
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
-    // ) {
-    //     struct Data {
-    //         a: u32,
-    //         b: u32,
-    //         c: u32,
-    //         d: u32,
-    //     }
-    //     add_data(Data{a,b,c,d});
-    //     #[tokio::test]
-    //     async fn it_works() {
-    //         let Data {
-    //             a,b,c,d
-    //         } = take_data();
-    //         let result = add(2, 2);
-    //         assert_eq!(result, 4);
-    //     }
-    //     it_works()
-    // }
-
     #[rstest]
-    #[tokio::test]
-    async fn slow(
+    #[allow(unnameable_test_items)]
+    fn fast(
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
     ) {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        struct Data {
+            a: u32,
+            b: u32,
+            c: u32,
+            d: u32,
+        }
+        add_data(Data{a,b,c,d});
+        #[tokio::test]
+        async fn it_works() {
+            let Data {
+                a,b,c,d
+            } = take_data();
+            let result = add(2, 2);
+            assert_eq!(result, 4);
+        }
+        it_works()
     }
+
+    // #[rstest]
+    // #[tokio::test]
+    // async fn slow(
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
+    // ) {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
