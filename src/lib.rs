da3104bfae8411236fdf9967e48e61331cba4a7a
michele <michele.damico@gmail.com>
2023-03-19 18:29:46 +0100

    First try

---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{any::Any, collections::HashMap, sync::Mutex};
+
+    use rstest::rstest;
+
+    use super::*;
+    lazy_static::lazy_static! {
+        static ref HASHMAP: Mutex<HashMap<String, Box<dyn Any + Send>>> = {
+            Mutex::new(HashMap::new())
+        };
+    }
+
+    fn tname() -> String {
+        std::thread::current().name().unwrap().to_string()
+    }
+
+    fn add_data<T: Send + 'static>(data: T) {
+        HASHMAP
+            .lock()
+            .unwrap()
+            .insert(tname().to_string(), Box::new(data));
+    }
+
+    fn take_data<T: 'static + Send>() -> T {
+        *HASHMAP
+            .lock()
+            .unwrap()
+            .remove(&tname())
+            .unwrap()
+            .downcast()
+            .unwrap()
+    }
+
+    // #[rstest]
+    // #[allow(unnameable_test_items)]
+    // fn fast(
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
+    // ) {
+    //     struct Data {
+    //         a: u32,
+    //         b: u32,
+    //         c: u32,
+    //         d: u32,
+    //     }
+    //     add_data(Data{a,b,c,d});
+    //     #[tokio::test]
+    //     async fn it_works() {
+    //         let Data {
+    //             a,b,c,d
+    //         } = take_data();
+    //         let result = add(2, 2);
+    //         assert_eq!(result, 4);
+    //     }
+    //     it_works()
+    // }
+
+    #[rstest]
+    #[tokio::test]
+    async fn slow(
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
+    ) {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
