32724582bfcbe409ac15a4d89c02b0fe72cc12dd
la10736 <michele.damico@gmail.com>
2023-03-21 14:34:33 +0100

    Add a first draft of thread local version

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,7 @@ mod tests {
     }
 
     fn add_data<T: Send + 'static>(data: T) {
-        HASHMAP
-            .lock()
-            .unwrap()
-            .insert(tname().to_string(), Box::new(data));
+        HASHMAP.lock().unwrap().insert(tname(), Box::new(data));
     }
 
     fn take_data<T: 'static + Send>() -> T {
@@ -37,8 +34,7 @@ mod tests {
     }
 
     #[rstest]
-    #[allow(unnameable_test_items)]
-    fn fast(
+    fn thread_local(
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
@@ -50,17 +46,53 @@ mod tests {
             c: u32,
             d: u32,
         }
-        add_data(Data{a,b,c,d});
-        #[tokio::test]
-        async fn it_works() {
-            let Data {
-                a,b,c,d
-            } = take_data();
-            let result = add(2, 2);
-            assert_eq!(result, 4);
+
+        thread_local! {
+            static DATA: Mutex<Option<Data>> = Mutex::new(None);
         }
-        it_works()
+        {
+            DATA.with(|r| {
+                *r.lock().unwrap() = Some(Data { a, b, c, d });
+            });
+        }
+        #[allow(unnameable_test_items)]
+        {
+            #[tokio::test]
+            async fn it_works() {
+                let Data { a, b, c, d } = DATA.with(|r| r.lock().unwrap().take().unwrap());
+                let result = add(2, 2);
+                assert_eq!(result, 4);
+            }
+            it_works()
+        }
     }
+
+    // #[rstest]
+    // #[allow(unnameable_test_items)]
+    // fn fast(
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
+    // ) {
+    //     struct Data {
+    //         a: u32,
+    //         b: u32,
+    //         c: u32,
+    //         d: u32,
+    //     }
+    //     add_data(Data { a, b, c, d });
+    //     #[allow(unnameable_test_items)]
+    //     {
+    //         #[tokio::test]
+    //         async fn it_works() {
+    //             let Data { a, b, c, d } = take_data();
+    //             let result = add(2, 2);
+    //             assert_eq!(result, 4);
+    //         }
+    //         it_works()
+    //     }
+    // }
 
     // #[rstest]
     // #[tokio::test]
