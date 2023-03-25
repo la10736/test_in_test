1a3b688d538a8b2f560b7bde21ced28715c4f667
la10736 <michele.damico@gmail.com>
2023-03-25 18:43:06 +0100

    Implement fast without hash table and lock. Moved to 100000 case add compile profile html files

--- src/lib.rs
+++ src/lib.rs
@@ -4,89 +4,43 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use std::{any::Any, collections::HashMap, sync::Mutex};
-
     use rstest::rstest;
 
     use super::*;
-    lazy_static::lazy_static! {
-        static ref HASHMAP: Mutex<HashMap<String, Box<dyn Any + Send>>> = {
-            Mutex::new(HashMap::new())
-        };
-    }
-
-    fn tname() -> String {
-        std::thread::current().name().unwrap().to_string()
-    }
-
-    fn add_data<T: Send + 'static>(data: T) {
-        HASHMAP.lock().unwrap().insert(tname(), Box::new(data));
-    }
-
-    fn take_data<T: 'static + Send>() -> T {
-        *HASHMAP
-            .lock()
-            .unwrap()
-            .remove(&tname())
-            .unwrap()
-            .downcast()
-            .unwrap()
-    }
-
-    #[rstest]
-    fn thread_local(
-        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
-        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
-        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
-        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
-    ) {
-        struct Data {
-            a: u32,
-            b: u32,
-            c: u32,
-            d: u32,
-        }
-
-        thread_local! {
-            static DATA: Mutex<Option<Data>> = Mutex::new(None);
-        }
-        {
-            DATA.with(|r| {
-                *r.lock().unwrap() = Some(Data { a, b, c, d });
-            });
-        }
-        #[allow(unnameable_test_items)]
-        {
-            #[tokio::test]
-            async fn it_works() {
-                let Data { a, b, c, d } = DATA.with(|r| r.lock().unwrap().take().unwrap());
-                let result = add(2, 2);
-                assert_eq!(result, 4);
-            }
-            it_works()
-        }
-    }
+    // Not too much gain in compilation but it's harder to implement due generics 
+    // definitions
+    // // struct Data {
+    // //     _a: u32,
+    // //     _b: u32,
+    // //     _c: u32,
+    // //     _d: u32,
+    // //     _e: u32,
+    // // }
 
     // #[rstest]
-    // #[allow(unnameable_test_items)]
-    // fn fast(
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
+    // fn thread_local(
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _a: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _b: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _c: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _d: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _e: u32,
     // ) {
     //     struct Data {
-    //         a: u32,
-    //         b: u32,
-    //         c: u32,
-    //         d: u32,
+    //         _a: u32,
+    //         _b: u32,
+    //         _c: u32,
+    //         _d: u32,
+    //         _e: u32,
     //     }
-    //     add_data(Data { a, b, c, d });
+    //     thread_local! {
+    //         static DATA: std::cell::RefCell<Option<Data>>  = std::cell::RefCell::new(None);
+    //     }
+    //     DATA.with(|r| r.replace(Some(Data { _a, _b, _c, _d, _e })));
     //     #[allow(unnameable_test_items)]
     //     {
     //         #[tokio::test]
     //         async fn it_works() {
-    //             let Data { a, b, c, d } = take_data();
+    //             let Data { _a, _b, _c, _d, _e } = DATA.with(|r| r.replace(None)).unwrap();
     //             let result = add(2, 2);
     //             assert_eq!(result, 4);
     //         }
@@ -94,15 +48,16 @@ mod tests {
     //     }
     // }
 
-    // #[rstest]
-    // #[tokio::test]
-    // async fn slow(
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] a: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] b: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] c: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] d: u32,
-    // ) {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
+    #[rstest]
+    #[tokio::test]
+    async fn slow(
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _a: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _b: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _c: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _d: u32,
+        #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _e: u32,
+    ) {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
