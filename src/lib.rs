b22b8a938b459a9dd650159586061dec8df101ff
michele <michele.damico@gmail.com>
2023-03-26 10:50:55 +0200

    Introduce a wrap that capture the state wrapper abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -17,47 +17,76 @@ mod tests {
     // //     _e: u32,
     // // }
 
-    // #[rstest]
-    // fn thread_local(
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _a: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _b: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _c: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _d: u32,
-    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _e: u32,
-    // ) {
-    //     struct Data {
-    //         _a: u32,
-    //         _b: u32,
-    //         _c: u32,
-    //         _d: u32,
-    //         _e: u32,
-    //     }
-    //     thread_local! {
-    //         static DATA: std::cell::RefCell<Option<Data>>  = std::cell::RefCell::new(None);
-    //     }
-    //     DATA.with(|r| r.replace(Some(Data { _a, _b, _c, _d, _e })));
-    //     #[allow(unnameable_test_items)]
-    //     {
-    //         #[tokio::test]
-    //         async fn it_works() {
-    //             let Data { _a, _b, _c, _d, _e } = DATA.with(|r| r.replace(None)).unwrap();
-    //             let result = add(2, 2);
-    //             assert_eq!(result, 4);
-    //         }
-    //         it_works()
-    //     }
-    // }
-
     #[rstest]
-    #[tokio::test]
-    async fn slow(
+    fn thread_local(
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _a: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _b: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _c: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _d: u32,
         #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _e: u32,
     ) {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        #[allow(unnameable_test_items)]
+        {
+            struct Data {
+                _a: u32,
+                _b: u32,
+                _c: u32,
+                _d: u32,
+                _e: u32,
+            }
+            thread_local! {
+                static DATA: std::cell::RefCell<Option<Data>>  = std::cell::RefCell::new(None);
+            }
+            DATA.with(|r| r.replace(Some(Data { 
+                _a, 
+                _b, 
+                _c, 
+                _d, 
+                _e 
+            })));
+            #[inline]
+            #[tokio::test]
+            async fn wrap (){
+                let Data { 
+                    _a, 
+                    _b, 
+                    _c, 
+                    _d, 
+                    _e 
+                } = DATA.with(|r| r.replace(None)).unwrap();
+                it_works(
+                    _a,
+                    _b,
+                    _c,
+                    _d,
+                    _e
+                ).await
+            }
+            wrap()
+        }
+
+        async fn it_works(
+            _a: u32,
+            _b: u32,
+            _c: u32,
+            _d: u32,
+            _e: u32,
+        ) {
+            let result = add(2, 2);
+            assert_eq!(result, 4);
+        }
     }
+
+    // #[rstest]
+    // #[tokio::test]
+    // async fn slow(
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _a: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _b: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _c: u32,
+    //     #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _d: u32,
+    //     // #[values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] _e: u32,
+    // ) {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
